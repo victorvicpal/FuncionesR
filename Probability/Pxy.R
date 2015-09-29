7b4425737eb804053b107d9f4358eb214bd40de9
Victor Vicente Palacios <victorvicpal@gmail.com>
2015-09-29 19:38:11 +0200

    Probability

---
+++ Probability/Pxy.R
@@ -0,0 +1,37 @@
+Pxy <- function(player,season,x,y)
+{
+	require(Coeficientes)
+	
+	#Coefficients
+	coef <- Coeficientes(player,season)
+
+	#Grid xy
+	A <- expand.grid(x,y)
+	names(A) <- c('x','y')
+
+
+	x <- A$x
+	y <- A$y
+	triple <- rep(0,length(x))
+
+	for (i in length(triple)) 
+	{
+		if(x[i]<3 | x[i]>47)
+		{
+			triple <- 1
+		}
+		else
+		{
+			if (y[i]>sqrt((23.71^2)-(x[i]-25)^2)+5.25)
+			{
+				triple <- 1
+			}
+		}
+	}
+
+	#Probability
+	Prob <- coef$"(Intercept)" + coef$"x"*x + coef$"y"*y + coef$"x:y"*x*y + coef$"I(x^2)"*x^2 + coef$"I(y^2)"*y^2 + triple*(coef$"triple1:x"*x + coef$"triple1:y"*y + coef$"triple1:x:y"*x*y + coef$"triple1:I(x^2)"*x^2 + coef$"triple1:I(y^2)"*y^2)
+	p <- exp(Prob)/(1 + exp(Prob))
+
+	C <- cbind.data.frame(x,y,p)
+}
